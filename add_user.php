284982bf7d246a3b9da1dc185614df1e061d9b47
swopet <swopet@oregonstate.edu>
2016-01-31 21:35:02 -0800

    Create add_user.php

---
+++ add_user.php
@@ -0,0 +1,14 @@
+<?php include("_header.php");?>
+
+<h1>Register</h1>
+
+<form method="post" action='add_user_receive.php' class="inform">
+<ul>
+<li><label>Username:</label> <input type="text" name="username">
+<li><label>Password:</label> <input type="password" name="password">
+<li><input type=submit>
+</ul>
+</form>
+
+
+<?php include("_footer.php");?>
