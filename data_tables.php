ae790468e9462c9adf48bbb4493bdb45d72fe6f2
John <magenhej@onid.oregonstate.edu>
2016-02-03 20:14:06 -0800

    Update data_tables.php
    Added 'password' column to the Users data table.
    Changed the database to leonl-db.

--- data_tables.php
+++ data_tables.php
@@ -1,20 +1,20 @@
 <?php
-$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "magenhej-db", "4hc69Q90z59znvKw", "magenhej-db");
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "leonl-db", "mCvXbcy9WsvzmzJ9", "leonl-db");
 
 
 $mysqli->query("drop table Users");
 $mysqli->query("drop table Pictures");
 
 
-if (!$mysqli->query("create table Users(uid integer, ref varchar(64), score integer, primary key(uid))")
+if (!$mysqli->query("create table Users(uid integer, username varchar(64), password varchar(64), score integer, primary key(uid))")
  || !$mysqli->query("create table Pictures(pid integer, name varchar(200), pts integer, hs integer, primary key(pid))")){
     printf("Cannot create table(s).\n");
 }
 
-$mysqli->query("insert into Users(uid,ref,score) values(932446782,'IanMage',764)");
-$mysqli->query("insert into Users(uid,ref,score) values(932784747,'loser',0)");
-$mysqli->query("insert into Users(uid,ref,score) values(932675478,'Louis',967)");
-$mysqli->query("insert into Users(uid,ref,score) values(932089506,'SuperWhiteSpaceWinnerMaster',990849)");
+$mysqli->query("insert into Users(uid,username,password,score) values(932446782,'IanMage', 'pwdfje53', 764)");
+$mysqli->query("insert into Users(uid,username,password,score) values(932784747,'loser', 'dsfgre5', 0)");
+$mysqli->query("insert into Users(uid,username,password,score) values(932675478,'Louis', 'dferyr7', 967)");
+$mysqli->query("insert into Users(uid,username,password,score) values(932089506,'SuperWhiteSpaceWinnerMaster', 'srghery9', 990849)");
 
 $mysqli->query("insert into Pictures(pid,name,pts,hs) values(1,'coke',800, 700)");
 $mysqli->query("insert into Pictures(pid,name,pts,hs) values(2,'pepsi',800, 750)");
@@ -23,11 +23,11 @@ $mysqli->query("insert into Pictures(pid,name,pts,hs) values(4,'herhsey',800, 78
 
 
 echo "<table>";
-if ($result = $mysqli->query("select uid,ref,score from Users")) {
+if ($result = $mysqli->query("select uid,username,score from Users")) {
     while($obj = $result->fetch_object()){ 
             echo "<tr>";
             echo "<td>".htmlspecialchars($obj->uid)."</td>"; 
-            echo "<td>".htmlspecialchars($obj->ref)."</td>"; 
+            echo "<td>".htmlspecialchars($obj->username)."</td>"; 
             echo "<td>".htmlspecialchars($obj->score)."</td>";  
             echo "</tr>";
     } 
