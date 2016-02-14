fed5f9dd136c5b005ca18894aefb55a0075ecbc3
swopet <swopet@oregonstate.edu>
2016-02-14 12:27:28 -0800

    Update init.php

--- init.php
+++ init.php
@@ -6,7 +6,7 @@ if (isset($_REQUEST["init"]) && $_REQUEST["init"] == "134891203489239") {
 
 
 $mysqli->query("drop table users");
-$mysqli->query("drop table images")
+$mysqli->query("drop table images");
 
 if (!$mysqli->query("create table users(uid integer NOT NULL AUTO_INCREMENT, username varchar(64), password varchar(512), salt varchar(512), primary key(uid) )")
 ||
