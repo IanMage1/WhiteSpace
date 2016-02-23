9020527a52bf756448f88849875030f2dd80c50c
swopet <swopet@oregonstate.edu>
2016-02-23 15:26:20 -0800

    Update init.php

--- init.php
+++ init.php
@@ -10,7 +10,7 @@ $mysqli->query("drop table images");
 
 if (!$mysqli->query("create table users(uid integer NOT NULL AUTO_INCREMENT, username varchar(64), password varchar(512), salt varchar(512), primary key(uid) )")
 ||
-!$mysqli->query("create table images(iid integer NOT NULL AUTO_INCREMENT, name varchar(64), address varchar(64), width integer, height integer, xtiles integer, ytiles integer, primary key(iid) )")) {
+!$mysqli->query("create table images(iid integer NOT NULL AUTO_INCREMENT, image mediumblob, name varchar(64), address varchar(64), width integer, height integer, xtiles integer, ytiles integer, primary key(iid) )")) {
     printf("Cannot create table(s).\n");
 }
 
