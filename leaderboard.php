33eddc346f6a421a18429a92bafd17f8ee2dd84d
John <magenhej@onid.oregonstate.edu>
2016-02-28 16:18:32 -0800

    Update Leaderboard
    Users table now holds 1,000+ users!

--- leaderboard.php
+++ leaderboard.php
@@ -9,19 +9,35 @@ $mysqli->query("drop table Users");
 
 
 
-if (!$mysqli->query("create table Users(uid integer, username varchar(64), password varchar(64), score integer, primary key(uid))")) {
+if (!$mysqli->query("create table Users(uid integer, username varchar(64), password varchar(64), score integer, currScore integer, primary key(uid))")) {
     printf("Cannot create table(s).\n");
 }
 
 
 //Users dummy data:
-$mysqli->query("insert into Users(uid,username,password,score) values(932446782,'John', 'magenhej', 764)");
-$mysqli->query("insert into Users(uid,username,password,score) values(932784747,'loser', 'losern', 0)");
-$mysqli->query("insert into Users(uid,username,password,score) values(932675478,'Louis', 'leonl', 967)");
-$mysqli->query("insert into Users(uid,username,password,score) values(932089506,'SuperWhiteSpaceWinnerMaster', 'superm', 990849)");
-$mysqli->query("insert into Users(uid,username,password,score) values(932907748,'Andrew', 'soltesza', 980)");
-$mysqli->query("insert into Users(uid,username,password,score) values(932094459,'William', 'selbiew', 960)");
-$mysqli->query("insert into Users(uid,username,password,score) values(932345345, 'Trevor', 'swopet', 1000)");
+$mysqli->query("insert into Users(uid,username,password,score,currScore) values(932446782,'John', 'magenhej', 764, 0)");
+$mysqli->query("insert into Users(uid,username,password,score,currScore) values(932784747,'loser', 'losern', 0, 0)");
+$mysqli->query("insert into Users(uid,username,password,score,currScore) values(932675478,'Louis', 'leonl', 967, 0)");
+$mysqli->query("insert into Users(uid,username,password,score,currScore) values(932089506,'SuperWhiteSpaceWinnerMaster', 'superm', 990849, 0)");
+$mysqli->query("insert into Users(uid,username,password,score,currScore) values(932907748,'Andrew', 'soltesza', 980, 0)");
+$mysqli->query("insert into Users(uid,username,password,score,currScore) values(932094459,'William', 'selbiew', 960, 0)");
+$mysqli->query("insert into Users(uid,username,password,score,currScore) values(932345345, 'Trevor', 'swopet', 1000, 0)");
+
+
+if ($stmt = $mysqli->prepare("insert into Users(uid,username,password,score,currScore) values(?,?,?,?,?)")) {
+  for ($uid = 1; $uid < 1000; $uid++) {
+    $username = "uid" . $uid;
+    $password =  'password';
+    $score = 500;
+    $currScore = 0;
+    $stmt->bind_param("issii", $uid, $username, $password, $score, $currScore);
+    $stmt->execute();
+  }
+  $stmt->close();
+} else {
+  printf("Error: %s\n", $mysqli->error);
+}
+
 
 
 echo "<table>";
@@ -40,6 +56,21 @@ echo "</table>";
 
 
 
+//<script>
+//setInterval(displayTable(){
+//    alert("It worked.");
+//}, 10000);
+//</script>
+
+
+//<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+//<script>
+//$( "#result" ).load( "/leaderboard.php", displayTable() {
+//    alert( "Table was displayed.");
+//});
+//</script>
 
 $mysqli->close();
+
+
 ?>
