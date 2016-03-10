3d75c00eb9256ba55802890eeb87828e1b37bf39
John <magenhej@onid.oregonstate.edu>
2016-03-09 18:48:07 -0800

    Update leaderboard.php
    Upon visiting the leaderboard page, the leaderboard now loads instantly.

--- leaderboard.php
+++ leaderboard.php
@@ -1,11 +1,13 @@
 <?php include("_header.php");?>
 
 <h1>Leaderboard</h1>
-<h4>(The leaderboard updates every 2 seconds)</h4>
+<h4>(Leaderboard updates every 2 seconds)</h4>
 
 <script src="https://code.jquery.com/jquery-1.12.0.js" type="text/javascript"></script>
+
 <script>
 $(document).ready(function(){
+	$("#screen").load('leaderboard2.php')
   setInterval(function(){
     $("#screen").load('leaderboard2.php')
   }, 2000);
